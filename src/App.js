c8023b3de57f2f2168ed21766ec28365ab9fe534
Steffano da Cruz <steffanodacruz@outlook.com>
2024-10-16 12:05:31 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+
+  const handlePrevious = function () {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  };
+
+  const handleNext = function () {
+    if (step < 3) {
+      setStep(step + 1);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="steps-box flex">
+        <div className={`step ${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`step ${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`step ${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="messages heavy center">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons-box flex">
+        <button
+          className="button heavy"
+          style={{ color: "#fff", backgroundColor: "#7950f2" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          className="button heavy"
+          style={{ color: "#fff", backgroundColor: "#7950f2" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
