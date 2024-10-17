7f03b1d20c933cf8d126f927e269b652264a5d8c
Steffano da Cruz <steffanodacruz@outlook.com>
2024-10-17 10:55:07 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,45 +8,54 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   const handlePrevious = function () {
     if (step > 1) {
-      setStep(step - 1);
+      setStep((s) => s - 1);
     }
   };
 
   const handleNext = function () {
     if (step < 3) {
-      setStep(step + 1);
+      setStep((s) => s + 1);
     }
   };
 
   return (
-    <div className="container">
-      <div className="steps-box flex">
-        <div className={`step ${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`step ${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`step ${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
-      <p className="messages heavy center">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons-box flex">
-        <button
-          className="button heavy"
-          style={{ color: "#fff", backgroundColor: "#7950f2" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          className="button heavy"
-          style={{ color: "#fff", backgroundColor: "#7950f2" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="container">
+          <div className="steps-box flex">
+            <div className={`step ${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`step ${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`step ${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="messages heavy center">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons-box flex">
+            <button
+              className="button heavy"
+              style={{ color: "#fff", backgroundColor: "#7950f2" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              className="button heavy"
+              style={{ color: "#fff", backgroundColor: "#7950f2" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
